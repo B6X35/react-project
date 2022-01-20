51b9c3a3a7c77734078a92b65ea5c4a0d83b0ab6
Istomina Alena <mynikisalena@gmail.com>
2022-01-20 23:21:02 +0200

    Add LoginPage and RegistrationPage components with Formik

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import './App.css';
+import { Switch, Route } from 'react-router-dom';
+// import { lazy, Suspense } from 'react';
 
+import RegistrationPage from './pages/RegistrationPage';
+import LoginPage from './pages/LoginPage';
+
+// import s from './App.module.css';
 function App() {
-  return <div className="container"></div>;
+  return (
+    <div>
+      {/* <Suspense fallback={<div>Loading...</div>}> */}
+      <Switch>
+        <Route exact path="/registration">
+          <RegistrationPage />
+        </Route>
+
+        <Route path="/login">
+          <LoginPage />
+        </Route>
+      </Switch>
+      {/* </Suspense> */}
+    </div>
+  );
 }
 
 export default App;
--- src/components/Shared components/Button/Button.jsx
+++ src/components/Shared components/Button/Button.jsx
@@ -1,10 +1,14 @@
 import s from './Button.module.css';
 
-const Button = ({ text = 'My text', isActive = true }) => {
+const Button = ({ text = 'My text', isActive = true, type = 'button' }) => {
   const element = isActive ? (
-    <button className={s.btnActive}> {text} </button>
+    <button className={s.btnActive} type={type}>
+      {text}
+    </button>
   ) : (
-    <button className={s.btnPassive}>{text} </button>
+    <button className={s.btnPassive} type={type}>
+      {text}
+    </button>
   );
 
   return element;
