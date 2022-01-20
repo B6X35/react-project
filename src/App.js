55e329c678f5ac89496cd61cf4d2cad027134c2f
Vlad <64969216+dreamkaman@users.noreply.github.com>
2022-01-20 15:49:20 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import './App.css';
-import Button from './components/Shared components/Button';
 
 function App() {
-  // const text = '';
-  return (
-    <div className="App">
-      <Button />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
