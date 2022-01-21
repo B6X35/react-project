d021f68cfc51472c6c50dd9e5fdf097cfce8293f
B6X35 <88390672+B6X35@users.noreply.github.com>
2022-01-21 10:37:37 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
+
 import s from './App.module.css';
 import './App.css';
 
-// import s from './App.module.css';
-
-import './App.css';
-import s from './App.css'
 function App() {
 
   return <div className="container"></div>;
-
 
 }
 
