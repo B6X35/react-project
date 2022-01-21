1a7bb18a33d4ca3f6953250551eaa6b6c557eac2
B6X35 <88390672+B6X35@users.noreply.github.com>
2022-01-21 11:03:35 +0200

    Update Button.jsx

--- src/components/Shared components/Button/Button.jsx
+++ src/components/Shared components/Button/Button.jsx
@@ -1,12 +1,12 @@
 import s from './Button.module.css';
 
-const Button = ({ text = 'My text', isActive = true, type = 'button' }) => {
+const Button = ({ text = 'My text', isActive = true, typeBtn = 'button' }) => {
   const element = isActive ? (
-    <button className={s.btnActive} type={type} >
+    <button className={s.btnActive} type={typeBtn} >
       {text}
     </button>
   ) : (
-    <button className={s.btnPassive} type={type}>
+    <button className={s.btnPassive} type={typeBtn}>
       {text}
     </button>
   );
