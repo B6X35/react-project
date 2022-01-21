36f29a21b2145695e513a1b96843ddd4dda1013e
Vlad <64969216+dreamkaman@users.noreply.github.com>
2022-01-21 20:30:30 +0200

    Input search component

--- src/components/DiaryProductsListItem/DiaryProductsListItem.jsx
+++ src/components/DiaryProductsListItem/DiaryProductsListItem.jsx
@@ -3,17 +3,13 @@ import Input from '../SharedComponents/Input';
 const DiaryProductsListItem = ({ arr = [] }) => {
   return (
     <div>
-      {/* <select size="3" multiple name="hero">
-        <option>Выберите героя</option>
-        <option value="Чебурашка">Чебурашка</option>
-        <option selected value="Крокодил Гена">
-          Крокодил Гена
-        </option>
-        <option value="Шапокляк">Шапокляк</option>
-        <option value="Крыса Лариса">Крыса Лариса</option>
-      </select> */}
+      <Input id="data1" type="search" list="data" />
 
-      <Input type="search" />
+      <datalist id="data">
+        {arr.map(item => (
+          <option key={item} value={item} />
+        ))}
+      </datalist>
     </div>
   );
 };
--- src/components/SharedComponents/Input/Input.jsx
+++ src/components/SharedComponents/Input/Input.jsx
@@ -1,9 +1,19 @@
 import PropTypes from 'prop-types';
 import s from './Input.module.css';
 
-const Input = ({ type = 'text', name, value, placeholder = 'Hello world', pattern }) => {
+const Input = ({
+  type = 'text',
+  id,
+  name,
+  value,
+  placeholder = 'Hello world',
+  list = 'somelist',
+  pattern,
+}) => {
   return (
     <input
+      id={id}
+      list={list}
       className={s.input}
       type={type}
       name={name}
@@ -18,6 +28,7 @@ const Input = ({ type = 'text', name, value, placeholder = 'Hello world', patter
 export default Input;
 
 Input.propTypes = {
+  id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   placeholder: PropTypes.string,
   value: PropTypes.string.isRequired,
