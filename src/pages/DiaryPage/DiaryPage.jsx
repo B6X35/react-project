39b94aaf60f09038ae8fc5dbf2889abd289ad998
pekosk <rkostyk64@gmail.com>
2022-01-21 15:09:59 +0200

    import fix

--- src/pages/DiaryPage/DiaryPage.jsx
+++ src/pages/DiaryPage/DiaryPage.jsx
@@ -1,17 +1,17 @@
-import DiaryDateCalendare from '../../components/DiaryDateCalendare';
+import DiaryDateCalendare from '../../components/DiaryDateCalendar';
 import DiaryAddProduct from '../../components/DiaryAddProduct';
 import DiaryProductList from '../../components/DiaryProductList';
 
 import s from './DiaryPage.module.css';
 
 const DiaryPage = () => {
-    return (
-        <section>
-            <DiaryDateCalendare />
-            <DiaryAddProduct />
-            <DiaryProductList />
-        </section>
-    )
-}
+  return (
+    <section>
+      <DiaryDateCalendar />
+      <DiaryAddProduct />
+      <DiaryProductList />
+    </section>
+  );
+};
 
 export default DiaryPage;
