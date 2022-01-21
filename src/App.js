9f57ff1d1062bec6eddb09ff9aa9e195244d1842
Vlad <64969216+dreamkaman@users.noreply.github.com>
2022-01-22 01:02:40 +0200

    next

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
 
 import { Switch, Route } from 'react-router-dom';
+import LoginForm from './components/LoginForm';
+import DiaryProductsListItem from './components/DiaryProductsListItem';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <DiaryProductsListItem
+      // arr={[1, 12, 123, 1234, 12345, 1234, 512345, 12345, 12345, 12345, 12345, 12345, 12345]}
+      />
+    </div>
+  );
 }
 
 export default App;
--- src/components/DiaryProductsListItem/DiaryProductsListItem.jsx
+++ src/components/DiaryProductsListItem/DiaryProductsListItem.jsx
@@ -1,15 +1,29 @@
 import Input from '../SharedComponents/Input';
+import s from './DiaryProductsListItem.module.css';
+import image from '../../images/sprite/plus.svg';
 
 const DiaryProductsListItem = ({ arr = [] }) => {
   return (
-    <div>
-      <Input id="data1" type="search" list="data" />
+    <div className={s.wrapperAll}>
+      <div className={s.wrapper}>
+        <div className={s.item}>
+          <Input id="data1" type="search" list="data" name="productName" />
 
-      <datalist id="data">
-        {arr.map(item => (
-          <option key={item} value={item} />
-        ))}
-      </datalist>
+          <datalist id="data">
+            {arr.map(item => (
+              <option key={item} value={item} />
+            ))}
+          </datalist>
+        </div>
+        <div className={s.item}>
+          <Input id="data2" type="text" name="weight" />
+        </div>
+      </div>
+      <button className={s.btn} type="button">
+        <svg>
+          <use href={image} className={s.vectorUse} />
+        </svg>
+      </button>
     </div>
   );
 };
