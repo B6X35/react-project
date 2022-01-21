9a2746254bed0403767a3344d8dfe84a44d32f18
B6X35 <d.b.palamarchuk@gmail.com>
2022-01-21 15:40:10 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-import { Switch, Route } from 'react-router-dom';
+// import { Switch, Route } from 'react-router-dom';
 // import { lazy, Suspense } from 'react';
 
-import RegistrationPage from './pages/RegistrationPage';
-import LoginPage from './pages/LoginPage';
+// import RegistrationPage from './pages/RegistrationPage';
+// import LoginPage from './pages/LoginPage';
 
 // import s from './App.module.css';
 function App() {
--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 
-import { store } from './redux/store';
+// import { store } from './redux/store';
 
 import './fonts/VerdanaBold.woff2';
 import './fonts/verdana.woff2';
