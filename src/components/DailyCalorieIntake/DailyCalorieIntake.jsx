bcc5cee5e9c10a710fda5378cbe29fa71ee533ca
Annomaliiya <ivasyukanna@gmail.com>
2022-01-24 16:33:27 +0200

    dailyCaloriesIntake logic and adding it to Modal

--- src/components/DailyCalorieIntake/DailyCalorieIntake.jsx
+++ src/components/DailyCalorieIntake/DailyCalorieIntake.jsx
@@ -1,18 +1,25 @@
 import Button from '../SharedComponents/Button';
 import s from './DailyCalorieIntake.module.css';
 import { useSelector } from 'react-redux';
+import { getDailyRate, getForbiddenProducts } from '../../redux/dailyRate/dailyRateSelectors';
 
 const DailyCalorieIntake = () => {
+  const calories = Math.floor(useSelector(getDailyRate));
+  const forbiddenProducts = useSelector(getForbiddenProducts);
+
+  const forbiddenListToShow = forbiddenProducts.map((item, idx) => (
+    <li className={s.forbiddenProductsItem} key={item}>
+      {idx + 1 + '. '}
+      {item}
+    </li>
+  ));
+
   return (
     <div className={s.calorieIntakeWrapper}>
-      <p className={s.calories}>1800 ккал</p>
-      <p className={s.title}>Продукты, которые вам не рекомендуется употреблять</p>
-      <ol className={s.forbiddenProducts}>
-        <li className={s.forbiddenProductsItem}>Мучные продукты </li>
-        <li className={s.forbiddenProductsItem}>Молоко</li>
-        <li className={s.forbiddenProductsItem}>Красное мясо </li>
-        <li className={s.forbiddenProductsItem}>Копчености</li>
-      </ol>
+      <p className={s.title}>Ваша рекомендуемая суточная норма калорий составляет</p>
+      <p className={s.calories}>{calories} ккал</p>
+      <p className={s.subTitle}>Продукты, которые вам не рекомендуется употреблять</p>
+      <ul className={s.forbiddenProducts}>{forbiddenListToShow}</ul>
       <div className={s.btnWrp}>
         <Button text="Начать худеть" type="submit" />
       </div>
