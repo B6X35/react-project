d35208ed1292f439cad8510bb6d47b8f9cb3b844
Vlad <64969216+dreamkaman@users.noreply.github.com>
2022-01-24 19:45:04 +0200

    next

--- src/App.js
+++ src/App.js
@@ -1,45 +1,7 @@
 import './App.css';
 import MainPageView from './pages/MainPageView';
 function App() {
-  return (
-
-    <div className="container">
-      {/* <DailyCalorieIntake /> */}
-      {/* <DiaryProductsListItem />
-      <DiaryProductsList
-        arr={[
-          { id: 1, productWeight: 100, productTitle: 'meat', calories: 123 },
-          { id: 2, productWeight: 100, productTitle: 'meat', calories: 123 },
-          { id: 3, productWeight: 100, productTitle: 'meat', calories: 123 },
-          { id: 4, productWeight: 100, productTitle: 'meat', calories: 123 },
-          { id: 5, productWeight: 100, productTitle: 'meat', calories: 123 },
-          { id: 6, productWeight: 100, productTitle: 'meat', calories: 123 },
-          { id: 7, productWeight: 100, productTitle: 'meat', calories: 123 },
-          { id: 8, productWeight: 100, productTitle: 'meat', calories: 123 },
-          { id: 9, productWeight: 100, productTitle: 'meat', calories: 123 },
-          { id: 10, productWeight: 100, productTitle: 'meat', calories: 123 },
-          { id: 11, productWeight: 100, productTitle: 'meat', calories: 123 },
-          { id: 12, productWeight: 100, productTitle: 'meat', calories: 123 },
-          { id: 13, productWeight: 100, productTitle: 'meat', calories: 123 },
-        ]}
-      /> */}
-      {/* <RoundButton /> */}
-      {/* <Header /> */}
-      {/* <Suspense fallback={<div>Loading...</div>}> */}
-      {/* <Switch>
-        <Route exact path="/registration">
-          <RegistrationPage />
-        </Route>
-        <Route path="/login">
-          <LoginPage />
-        </Route>
-      </Switch> */}
-      {/* </Suspense> */}
-      {/* <DiaryPage /> */}
-      {/* <Modal/> */}
-    </div>
-
-  );
+  return <MainPageView />;
 }
 
 export default App;
