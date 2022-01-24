0e36ab99cfd72a57bab0c754c86eea9037e86399
Annomaliiya <ivasyukanna@gmail.com>
2022-01-24 14:58:20 +0200

    markup and styles for dailyCaloriesIntake

--- src/components/DailyCalorieIntake/DailyCalorieIntake.jsx
+++ src/components/DailyCalorieIntake/DailyCalorieIntake.jsx
@@ -1,5 +1,23 @@
+import Button from '../SharedComponents/Button';
+import s from './DailyCalorieIntake.module.css';
+import { useSelector } from 'react-redux';
+
 const DailyCalorieIntake = () => {
-  return <p>Hallo, World!</p>;
+  return (
+    <div className={s.calorieIntakeWrapper}>
+      <p className={s.calories}>1800 ккал</p>
+      <p className={s.title}>Продукты, которые вам не рекомендуется употреблять</p>
+      <ol className={s.forbiddenProducts}>
+        <li className={s.forbiddenProductsItem}>Мучные продукты </li>
+        <li className={s.forbiddenProductsItem}>Молоко</li>
+        <li className={s.forbiddenProductsItem}>Красное мясо </li>
+        <li className={s.forbiddenProductsItem}>Копчености</li>
+      </ol>
+      <div className={s.btnWrp}>
+        <Button text="Начать худеть" type="submit" />
+      </div>
+    </div>
+  );
 };
 
 export default DailyCalorieIntake;
