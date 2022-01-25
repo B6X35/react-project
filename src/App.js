9b32ba1f7b16ac08574a67f8c90f77d247f8e53d
Vlad <64969216+dreamkaman@users.noreply.github.com>
2022-01-25 19:07:20 +0200

    next

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
-
+import LoaderTailSpin from './components/Loader';
+//props={ dateOn, kcalLeft, kcalConsumed, dailyRate, percentsOfDailyRate, notAllowedProducts };
 function App() {
-  return;
+  return <LoaderTailSpin />;
 }
 
 export default App;
