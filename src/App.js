5484e0d01206dba5357970fee183a7b41b678027
Vlad <64969216+dreamkaman@users.noreply.github.com>
2022-01-25 14:56:33 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import './App.css';
-import Button from './components/SharedComponents/Button';
-
-//{ text = 'My text', isActive = true, type = 'button', pathName, onClick }
 
 function App() {
-  return <Button isActive={false} pathName={'/home'} />;
+  return;
 }
 
 export default App;
