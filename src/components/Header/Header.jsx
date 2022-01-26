796714b806b569fb08aa26a83e5a162d947909ee
levchenko1986 <vadimleva1986@gmail.com>
2022-01-26 19:28:23 +0200

    Header test

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -15,7 +15,7 @@ function Header() {
         </div>
         <NavAll />
       </div>
-      <div className={s.userInfoWrap}>{isLogin && <UserInfo />}</div>
+      <div className={s.userInfoHead}>{isLogin && <UserInfo />}</div>
     </div>
   );
 }
