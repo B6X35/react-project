e586ae6392e1bd0aae74f8c9cb33b50e2f3b70a7
pekosk <rkostyk64@gmail.com>
2022-01-27 12:59:27 +0200

    модалка

--- src/components/NavAll/NavAll.jsx
+++ src/components/NavAll/NavAll.jsx
@@ -1,25 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 // import routes from '../../Routers/Routes';
 import { useSelector } from 'react-redux';
 import NavPrivate from './NavPrivate/NavPrivate';
 import NavPublic from './NavPublic/NavPublic';
 import { getIsAuth } from '../../redux/auth/authSelectors';
 import s from './NavAll.module.css';
+import BurgerCloseButton from '../SharedComponents/BurgerCloseButton/BurgerCloseButton';
+import BurgerModal from '../BurgerModal';
 
 const NavAll = () => {
   const isLogin = useSelector(getIsAuth);
-  return (
 
+   const [isActive, setActive] = useState(false);
+  const toggleModal = () => {
+    setActive(!isActive);
+  }
+  return (
+<>
   <ul className={s.NavigateAll}>
     {isLogin ? (
-    // <li className={s.NavPrivate}>
-    <NavPrivate />)
-    // </li>  
+    <li className={s.NavPrivate}>
+    {/* // <NavPrivate /> */}
+    {isActive ? (<BurgerModal isActive={isActive}><NavPrivate/></BurgerModal>) : (<NavPrivate />)}
+     </li>  )
     // <li className={s.NavPublic}>
   : (  <NavPublic />) }
     {/* </li>*/}
   </ul>
-
+  <BurgerCloseButton isActive={isActive} onClick={toggleModal} />
+        {/* <BurgerModal isActive={isActive} /> */}
+</>
   );
 };
 // routes.map(route => {
