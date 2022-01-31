e043947f1a02ede02749ca7dd331bc095f494480
Vlad <64969216+dreamkaman@users.noreply.github.com>
2022-01-31 17:43:07 +0200

    Update authSlice.js

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -7,7 +7,7 @@ const initialState = {
     username: null,
   },
   todaySummary: {
-      userId: null,
+    userId: null,
   },
   currentUser: [],
   sid: null,
@@ -19,84 +19,86 @@ const initialState = {
 };
 
 const authSlice = createSlice({
-    name: 'auth',
-    initialState,
-    extraReducers: {
-        [registerUser.pending]: (state) => {
-            state.isLoading = true;
-            state.error = null;
-          },
-          [registerUser.fulfilled]: (state, { payload }) => {
-            state.isLoading = false;
-            state.isLoginUser = true;
-            state.token = payload.refreshToken;
-            state.sid = payload.sid;
-            state.user = {
-              email: payload.user.email,
-              username: payload.user.username,
-            };
-            state.todaySummary = {
-                userId: payload.userId,
-            }
-          },
-          [registerUser.rejected]: (state, { payload }) => {
-            state.isLoading = false;
-            state.error = payload;
-          },
-          [loginUser.pending]: (state) => {
-            state.isLoading = true;
-            state.error = null;
-          },
-          [loginUser.fulfilled]: (state, { payload }) => {
-            state.isLoading = false;
-            state.isLoginUser = true;
-            state.token = payload.refreshToken;
-            state.sid = payload.sid;
-            state.user = {
-              email: payload.user.email,
-              username: payload.user.username,
-            };
-            state.todaySummary = {
-                userId: payload.userId,
-            };
-          },
-          [loginUser.rejected]: (state, { payload }) => {
-            state.isLoading = false;
-            state.error = payload;
-          },
-          [currentUser.pending]: (state) => {
-            state.isLoading = true;
-            state.error = null;
-            state.isFetchCurrentUser = false
-          },
-          [currentUser.fulfilled]: (state, { payload }) => {
-            state.isLoading = false;
-            state.isFetchCurrentUser = false
-            state.isLoginUser = true;
-            state.user = payload;
-          },
-          [currentUser.rejected]: (state, { payload }) => {
-            state.isLoading = false;
-            state.error = payload;
-            state.isFetchCurrentUser = false
-          },
-          [logoutUser.pending]: (state) => {
-            state.isLoading = true;
-            state.error = null;
-          },
-          [logoutUser.fulfilled]: (state) => {
-            state.isLoading = false;
-            state.token = null;
-            state.isLoginUser = false;
-            state.user = {
-              email: null,
-              username: null
-            };
-          },
-          [logoutUser.rejected]: () => {
-            return initialState;
-          },
-    }
-})
+  name: 'auth',
+  initialState,
+  extraReducers: {
+    [registerUser.pending]: state => {
+      state.isLoading = true;
+      state.error = null;
+    },
+    [registerUser.fulfilled]: (state, { payload }) => {
+      state.isLoading = false;
+      state.isLoginUser = true;
+      state.token = payload.refreshToken;
+      state.sid = payload.sid;
+      state.user = {
+        email: payload.user.email,
+        username: payload.user.username,
+      };
+      state.todaySummary = {
+        userId: payload.userId,
+      };
+    },
+    [registerUser.rejected]: (state, { payload }) => {
+      state.isLoading = false;
+      state.error = payload;
+    },
+    [loginUser.pending]: state => {
+      state.isLoading = true;
+      state.error = null;
+    },
+    [loginUser.fulfilled]: (state, { payload }) => {
+      state.isLoading = false;
+      state.isLoginUser = true;
+      state.token = payload.refreshToken;
+      state.sid = payload.sid;
+      state.user = {
+        email: payload.user.email,
+        username: payload.user.username,
+        userData: { ...payload.user.userData }, //new prop
+      };
+      state.todaySummary = {
+        // userId: payload.userId,
+        ...payload.todaySummary,
+      };
+    },
+    [loginUser.rejected]: (state, { payload }) => {
+      state.isLoading = false;
+      state.error = payload;
+    },
+    [currentUser.pending]: state => {
+      state.isLoading = true;
+      state.error = null;
+      state.isFetchCurrentUser = false;
+    },
+    [currentUser.fulfilled]: (state, { payload }) => {
+      state.isLoading = false;
+      state.isFetchCurrentUser = false;
+      state.isLoginUser = true;
+      state.user = payload;
+    },
+    [currentUser.rejected]: (state, { payload }) => {
+      state.isLoading = false;
+      state.error = payload;
+      state.isFetchCurrentUser = false;
+    },
+    [logoutUser.pending]: state => {
+      state.isLoading = true;
+      state.error = null;
+    },
+    [logoutUser.fulfilled]: state => {
+      state.isLoading = false;
+      state.token = null;
+      state.isLoginUser = false;
+      state.user = {
+        email: null,
+        username: null,
+      };
+    },
+    [logoutUser.rejected]: () => {
+      return initialState;
+    },
+  },
+});
 
-export default authSlice.reducer
+export default authSlice.reducer;
